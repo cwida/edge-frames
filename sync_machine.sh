7c56ed352681457ebb8c208e8fb539558482167a
Per Fuchs <per@shiftleft.io>
2019-02-20 12:59:19 +0100

    Initial commit

---
+++ sync_machine.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+if [ "$#" -eq 1 ]; then
+    target_machine=$1
+    echo "Connecting to "$target_machine
+else
+    echo "Usage: ssh-machine [target_machine]"
+    exit 1
+fi
+
+user=perfuchs
+server_dir=/scratch/per/scala-test/
+
+dirs="target/scala-2.11/*.jar"
+
+for d in $dirs
+do
+        rsync -avh --exclude '.git*' $d $user@$target_machine:$server_dir
+done
+
+
+
+
+
+
+
