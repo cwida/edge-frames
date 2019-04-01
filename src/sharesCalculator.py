4fea0d5ec4bfd7e44e2213925ad2a4c27107b967
Per Fuchs <per@shiftleft.io>
2019-04-01 14:06:28 +0200

    Adds path, diamond and house pattern again.

--- src/sharesCalculator.py
+++ src/sharesCalculator.py
@@ -54,20 +54,41 @@ def clique_pattern(num_vertices):
       if v1 < v2:
         edges.append((v1, v2))
 
-  return (vertices, edges)
+  return vertices, edges
 
 
-def numberOfEdgesForPath(num_vertices):
-  return num_vertices - 1
+def path_pattern(num_vertices):
+  vertices = list(map(lambda i: ascii_lowercase[i], range(num_vertices)))
+  edges = []
+  for (v1, i) in enumerate(vertices):
+    if i < len(vertices) - 1:
+      edges.append((v1, vertices[i + 1]))
 
+  return vertices, edges
+
+
+def diamond_pattern():
+  clique = clique_pattern(4)
+  clique[1].remove(('a', 'd'))
+  return clique
+
+
+def house_pattern():
+  clique = clique_pattern(5)
+  clique[1].remove(('a', 'd'))
+  clique[1].remove(('a', 'e'))
+  return clique
 
 cliquePatterns = list(map(lambda i: clique_pattern(i), range(3, 7)))
+pathPatterns = list(map(lambda i: clique_pattern(i), range(2, 10)))
 
+patterns = cliquePatterns + pathPatterns + [diamond_pattern()] + [house_pattern()]
+print(patterns)
 fieldnames = ['vertices', 'edges', 'workers', 'workers_used', 'config', 'max_percentage']
 rows = []
 workers = 64
 
-for (v, e) in cliquePatterns:
+for (v, e) in patterns:
     c = best_configuration(workers, e, v)
     rows.append((len(v), len(e), workers, numberOfWorkers(c), '; '.join(map(str, map(lambda v: c[v], v))), workload_per_worker(e, c)))
 
