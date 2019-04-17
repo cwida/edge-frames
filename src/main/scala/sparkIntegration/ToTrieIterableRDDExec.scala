048fd6314415295cd328e89f52ad01c2ea91cdb4
Per Fuchs <per@shiftleft.io>
2019-04-17 12:17:53 +0200

    Adds timing metric to ToTrieIterableRDDExec

--- src/main/scala/sparkIntegration/ToTrieIterableRDDExec.scala
+++ src/main/scala/sparkIntegration/ToTrieIterableRDDExec.scala
@@ -5,11 +5,25 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.{Ascending, Attribute, SortOrder}
 import org.apache.spark.sql.execution.SparkPlan
+import org.apache.spark.sql.execution.metric.SQLMetrics
 
 case class ToTrieIterableRDDExec(child: SparkPlan) extends SparkPlan{
+  val MATERIALIZATION_TIME_METRIC = "materializationTime"
+
+  override lazy val metrics = Map(
+    MATERIALIZATION_TIME_METRIC -> SQLMetrics.createTimingMetric(sparkContext, "materialization time"))
+
   override protected def doExecute(): RDD[InternalRow] = {
+    val matTime = longMetric(MATERIALIZATION_TIME_METRIC)
+
     new TrieIterableRDD[ArrayTrieIterable](child.execute()
-      .mapPartitions(iter => Iterator(new ArrayTrieIterable(iter))))
+      .mapPartitions(iter => {
+        val start = System.nanoTime()
+        val ret = Iterator(new ArrayTrieIterable(iter))
+        val end = System.nanoTime()
+        matTime += (end - start) / 1000000
+        ret
+      }))
   }
 
   override def output: Seq[Attribute] = child.output
