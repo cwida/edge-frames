b70cb8219b01c96ccf5b71ffce0f5fe2fc2afbc5
Per Fuchs <per@shiftleft.io>
2019-04-17 17:32:27 +0200

    Allows to use cached datasets as input to WCOJ

--- src/main/scala/sparkIntegration/WCOJ.scala
+++ src/main/scala/sparkIntegration/WCOJ.scala
@@ -1,14 +1,21 @@
 package sparkIntegration
 
+import leapfrogTriejoin.TreeTrieIterator
 import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference, AttributeSet}
 import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, UnaryNode}
 import org.apache.spark.sql.types.{IntegerType, LongType}
+import org.slf4j.LoggerFactory
+
 import Predef._
 
 case class WCOJ(joinSpecification: JoinSpecification, children: Seq[LogicalPlan]) extends LogicalPlan {
+  private val logger = LoggerFactory.getLogger(classOf[WCOJ])
 
-  require(joinSpecification.edges.size == children.size, "WCOJ needs as many relationship as edges in the JoinSpecification")
-  require(children.forall(c => c.output.size == 2 && List("src", "dst").forall(s => c.output.map(_.name).contains(s))), "Each children of a WCOJ should have two attributes called src and dst") // TODO handle general case of more attributes.
+  logger.error(children.map(c => c.output.map(_.name).mkString(",")).mkString(";"))
+
+  // TODO if I cache the input dataset there is one stage in query planning in which the output is unnamed (none). Find a different place to require this.
+//  require(joinSpecification.edges.size == children.size, "WCOJ needs as many relationship as edges in the JoinSpecification")
+//  require(children.forall(c => c.output.size == 2 && List("src", "dst").forall(s => c.output.map(_.name).contains(s))), "Each children of a WCOJ should have two attributes called src and dst") // TODO handle general case of more attributes.
 
   override def references: AttributeSet = AttributeSet(children.flatMap(c => c.output.filter(a => List("src", "dst").contains(a.name))))
 
