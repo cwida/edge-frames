09c2f90b43c13be37f513506f70cf158aff0b6e5
Per Fuchs <per@shiftleft.io>
2019-04-23 10:46:21 +0200

    Adds helper to produce edge relationship that are "indexed" in both directions.

--- src/main/scala/sparkIntegration/WCOJFunctions.scala
+++ src/main/scala/sparkIntegration/WCOJFunctions.scala
@@ -1,7 +1,10 @@
 package org.apache.spark.sql
 
+import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.types.IntegerType
 import sparkIntegration.{JoinSpecification, Pattern, WCOJ}
+import org.apache.spark.sql.catalyst.encoders._
+import org.apache.spark.sql.execution.RowIterator
 
 import Predef._
 
@@ -16,11 +19,96 @@ class WCOJFunctions[T](ds: Dataset[T]) {
     val edges = Pattern.parse(pattern)
 
     val joinSpecification = new JoinSpecification(edges, variableOrdering)
-    val children = edges.zipWithIndex.map { case (p, i) => ds.alias(s"edges_${i.toString}")
-      .withColumnRenamed("src", s"src")  // Needed to guarantee that src and dst on the aliases are referenced by different attributes.
-      .withColumnRenamed("dst", s"dst")
-      .logicalPlan}
+    val children = edges.zipWithIndex.map { case (p, i) => {
+      ds.alias(s"edges_${i.toString}")
+        .withColumnRenamed("src", s"src") // Needed to guarantee that src and dst on the aliases are referenced by different attributes.
+        .withColumnRenamed("dst", s"dst")
+        .logicalPlan
+    }
+    }
 
     Dataset.ofRows(ds.sparkSession, new WCOJ(joinSpecification, children))
+  }
+
+  /**
+    * Creates an edge relationship from a dataset.
+    *
+    * The input dataset is required to have two integer attributes called `src` and `dst`.
+    * The returned dataset will be:
+    *   1. projected on these two attributes
+    *   2. if `allowArbritaryVariableOrderings` then each edge will exist in both directions
+    *   3. tagged with a boolean value which is true for edges from the original dataset
+    *   4. sorted by `src`, `dst` ASC.
+    *
+    * @param allowArbritaryVariableOrderings allows to query the dataset with arbitrary variable orderings in a WCOJ.
+    * @param isUndirected                    if `true` the dataset is assumed to be of an undirected graph with edges that exist only in one direction,
+    *                                        that saves some time during construction
+    * @param isUndirectedDuplicated          if `true` the dataset is assumed to be of an undirected graph with
+    *                                        edges in both direction already existing, which saves even more time during construction.
+    * @return
+    */
+  def toEdgeRelationship(allowArbritaryVariableOrderings: Boolean = true,
+                         isUndirected: Boolean = false,
+                         isUndirectedDuplicated: Boolean = false): Dataset[(Int, Int, Boolean)] = {
+    import ds.sparkSession.implicits._
+
+    require(ds.columns.contains("src"), "Edge table should have a column called `src`")
+    require(ds.columns.contains("dst"), "Edge table should have a column called `dst`")
+
+    require(ds.col("src").expr.dataType == IntegerType, "Edge table src needs to be an integer")
+    require(ds.col("dst").expr.dataType == IntegerType, "Edge table src needs to be an integer")
+
+    val projected = ds.select("src", "dst").as[(Int, Int)]
+    val duplicated: Dataset[(Int, Int, Boolean)] =
+      if (allowArbritaryVariableOrderings && !isUndirectedDuplicated) {
+        projected.flatMap { case (src, dst) => {
+          Seq((src, dst, true), (dst, src, false))
+        }
+        }
+      } else {
+        projected.map { case (src, dst) => {
+          (src, dst, true)
+        }
+        }
+      }
+
+    val sorted = duplicated.sort("_1", "_2")
+
+    if (allowArbritaryVariableOrderings && !isUndirected && !isUndirectedDuplicated) {
+      // Remove duplicates that existed already
+      sorted.mapPartitions(iter => {
+        new Iterator[(Int, Int, Boolean)] {
+          var lookBack: (Int, Int, Boolean) = if (iter.hasNext) {
+            iter.next()
+          } else {
+            null
+          }
+
+          override def hasNext: Boolean = {
+            iter.hasNext || lookBack != null
+          }
+
+          override def next(): (Int, Int, Boolean) = {
+            if (iter.hasNext) {
+              val n = iter.next()
+              if (n._1 == lookBack._1 && n._2 == lookBack._2) {
+                lookBack = null
+                (n._1, n._2, true)
+              } else {
+                val ret = lookBack
+                lookBack = n
+                ret
+              }
+            } else {
+              val temp = lookBack // Cannot be null because `hasNext` returned `true`
+              lookBack = null
+              temp
+            }
+          }
+        }
+      })
+    } else {
+      sorted
+    }.withColumnRenamed("_1", "src").withColumnRenamed("_1", "dst").as[(Int, Int, Boolean)]
   }
 }
---
+++ src/test/scala/sparkIntegration/ToEdgeRelationTest.scala
@@ -0,0 +1,84 @@
+package sparkIntegration
+
+import leapfrogTriejoin.ArrayTrieIterable
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.scalacheck.Gen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import sparkIntegration.implicits._
+
+class ToEdgeRelationTest extends FlatSpec with Matchers with BeforeAndAfterAll with GeneratorDrivenPropertyChecks {
+  val sp: SparkSession = setupSpark()
+
+  def setupSpark(): SparkSession = {
+    val conf = new SparkConf()
+      .setMaster("local[1]")
+      .setAppName("Spark test")
+      .set("spark.executor.memory", "2g")
+      .set("spark.driver.memory", "2g")
+      .set("spark.default.parallelism", "1")
+
+    val spark = SparkSession.builder()
+      .config(conf)
+      .getOrCreate()
+
+    spark.experimental.extraStrategies = (Seq(WCOJ2WCOJExec) ++ spark.experimental.extraStrategies)
+    spark
+  }
+
+  "ToEdgeRelation" should "add edges in the other direction" in {
+    import sp.implicits._
+    val ds = sp.sparkContext.parallelize(Seq((1,2))).toDF("src", "dst").as[(Int, Int)]
+
+    val edgeRel = ds.toEdgeRelationship()
+
+    edgeRel.collect() should contain theSameElementsAs Seq((1, 2, true), (2, 1, false))
+  }
+
+  "ToEdgeRelation" should "not add existing edges as copy again" in {
+    import sp.implicits._
+    val ds = sp.sparkContext.parallelize(Seq((1,2), (2, 1))).toDF("src", "dst").as[(Int, Int)]
+
+    val edgeRel = ds.toEdgeRelationship()
+
+    edgeRel.collect() should contain theSameElementsAs Seq((1, 2, true), (2, 1, true))
+  }
+
+  "ToEdgeRelation" should "sort by src and dst" in {
+    import sp.implicits._
+    val ds = sp.sparkContext.parallelize(Seq((2,3), (2, 1), (1, 3))).toDF("src", "dst").as[(Int, Int)]
+
+    val edgeRel = ds.toEdgeRelationship()
+
+    edgeRel.collect() should contain theSameElementsInOrderAs Seq((1, 2, false), (1, 3, true), (2, 1, true), (2, 3, true), (3, 1, false), (3, 2, false))
+  }
+
+
+  "ToEdgeRelation" should "sort by src and dst, add edges in the opposite direction, not duplicate existing edges (property based)" in {
+    import sp.implicits._
+
+    val positiveIntTuples = Gen.buildableOf[Set[(Int, Int)], (Int, Int)](Gen.zip(Gen.posNum[Int], Gen.posNum[Int]))
+
+    forAll (positiveIntTuples) { l =>
+      whenever(l.forall(t => t._1 > 0 && t._2 > 0)) {  // Sad way to ensure numbers are actually positive
+        val expectedOutput = l.toArray.flatMap(
+          { case (src, dst) => {
+            if (l.contains((dst, src))) {
+              Seq((src, dst, true))
+            } else {
+              Seq((src, dst, true), (dst, src, false))
+            }
+          }}).sorted
+
+        val ds: Dataset[(Int, Int)] = sp.sparkContext.parallelize(l.toSeq).toDF("src", "dst").as[(Int, Int)]
+
+        val edgeRel = ds.toEdgeRelationship()
+
+        edgeRel.collect should contain theSameElementsInOrderAs expectedOutput
+
+      }
+    }
+
+  }
+}
