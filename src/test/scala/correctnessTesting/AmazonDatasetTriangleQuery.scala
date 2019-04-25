c4dfbae15c5034284b0ea0d3da223d8958f56ecf
Per Fuchs <per@shiftleft.io>
2019-04-25 13:56:51 +0200

    Adds 3 and 4-path queries.
    Path queries so far should not be run on the whole dataset as it takes to long.

--- src/test/scala/correctnessTesting/AmazonDatasetTriangleQuery.scala
+++ src/test/scala/correctnessTesting/AmazonDatasetTriangleQuery.scala
@@ -1,7 +1,7 @@
 package correctnessTesting
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.scalatest.{FlatSpec, Matchers}
 import sparkIntegration.WCOJ2WCOJExec
 import sparkIntegration.implicits._
@@ -46,7 +46,7 @@ class AmazonDatasetTriangleQuery extends FlatSpec with Matchers with SparkTest {
       .repartition(1)
       .cache()
     if (FAST) {
-      df.limit(200)
+      df.limit(1000)
     } else {
       df
     }
@@ -65,26 +65,80 @@ class AmazonDatasetTriangleQuery extends FlatSpec with Matchers with SparkTest {
 
   def twoPathNodeSet(rel: DataFrame): (DataFrame, DataFrame) = {
     (rel.selectExpr("src AS a").sample(0.1, FIXED_SEED_1).cache(),
-      rel.selectExpr("src AS c").sample(0.1, FIXED_SEED_2).cache())
+      rel.selectExpr("src AS z").sample(0.1, FIXED_SEED_2).cache())
+  }
+
+  def withDistincColumns(df: DataFrame, cols: Seq[String]): DataFrame = {
+    var r = df
+    for (c <- cols.combinations(2)) {
+      r = r.where(new Column(c(0)) !== new Column(c(1)))
+    }
+    r
   }
 
   def twoPathGoldStandard(rel: DataFrame, nodeSet1: DataFrame, nodeSet2: DataFrame): DataFrame = {
     val relLeft = rel.selectExpr("src AS a", "dst AS b").join(nodeSet1, Seq("a"), "left_semi")
-    val relRight = rel.selectExpr("dst AS c", "src AS b").join(nodeSet2, Seq("c"), "left_semi")
+    val relRight = rel.selectExpr("dst AS z", "src AS b").join(nodeSet2, Seq("z"), "left_semi")
 
-    relLeft.join(relRight, Seq("b")).selectExpr("a", "b", "c")
+    withDistincColumns(relLeft.join(relRight, Seq("b")).selectExpr("a", "b", "z"), Seq("a", "b", "z"))
+
   }
 
   def twoPathPattern(rel: DataFrame, nodeSet1: DataFrame, nodeSet2: DataFrame) = {
     val twoPath = rel.findPattern(
       """
         |(a) - [] -> (b);
-        |(b) - [] -> (c)
-      """.stripMargin, Seq("a", "c", "b"))
+        |(b) - [] -> (z)
+      """.stripMargin, Seq("a", "z", "b"))
     // TODO should be done before the join
-    twoPath.join(nodeSet1, Seq("a"), "left_semi")
-      .join(nodeSet2, Seq("c"), "left_semi")
-      .select("a", "b", "c")
+    val filtered = twoPath.join(nodeSet1, Seq("a"), "left_semi")
+      .join(nodeSet2, Seq("z"), "left_semi")
+      .select("a", "b", "z")
+    withDistincColumns(filtered, Seq("a", "b", "z"))
+  }
+
+  def threePathGoldStandard(rel: DataFrame, nodeSet1: DataFrame, nodeSet2: DataFrame): DataFrame = {
+    val relLeft = rel.selectExpr("src AS a", "dst AS b").join(nodeSet1, Seq("a"), "left_semi")
+    val relRight = rel.selectExpr("dst AS z", "src AS c").join(nodeSet2, Seq("z"), "left_semi")
+
+    val middleLeft = relLeft.join(rel.selectExpr("src AS b", "dst AS c"), Seq("b")).selectExpr("a", "b", "c")
+    relRight.join(middleLeft, "c").select("a", "b", "c", "z")
+  }
+
+  def threePathPattern(rel: DataFrame, nodeSet1: DataFrame, nodeSet2: DataFrame) = {
+    val threePath = rel.findPattern(
+      """
+        |(a) - [] -> (b);
+        |(b) - [] -> (c);
+        |(c) - [] -> (z)
+      """.stripMargin, Seq("a", "z", "c", "b"))
+    // TODO should be done before the join
+    threePath.join(nodeSet1, Seq("a"), "left_semi")
+      .join(nodeSet2, Seq("z"), "left_semi")
+      .select("a", "b", "c", "z")
+  }
+
+  def fourPathGoldStandard(rel: DataFrame, nodeSet1: DataFrame, nodeSet2: DataFrame): DataFrame = {
+    val relLeft = rel.selectExpr("src AS a", "dst AS b").join(nodeSet1, Seq("a"), "left_semi")
+    val relRight = rel.selectExpr("dst AS z", "src AS d").join(nodeSet2, Seq("z"), "left_semi")
+
+    val middleLeft = relLeft.join(rel.selectExpr("src AS b", "dst AS c"), Seq("b")).selectExpr("a", "b", "c")
+    val middleRight = relRight.join(rel.selectExpr("src AS c", "dst AS d"), Seq("d")).selectExpr("c", "d", "z")
+    withDistincColumns(middleRight.join(middleLeft, "c").select("a", "b", "c", "d", "z"), Seq("a", "b", "c", "d", "z"))
+  }
+
+  def fourPathPattern(rel: DataFrame, nodeSet1: DataFrame, nodeSet2: DataFrame) = {
+    val fourPath = rel.findPattern(
+      """
+        |(a) - [] -> (b);
+        |(b) - [] -> (c);
+        |(c) - [] -> (d);
+        |(d) - [] -> (z)
+      """.stripMargin, Seq("a", "z", "c", "b", "d"))
+    // TODO should be done before the join
+    withDistincColumns(fourPath.join(nodeSet1, Seq("a"), "left_semi")
+      .join(nodeSet2, Seq("z"), "left_semi")
+      .select("a", "b", "c", "d", "z"), Seq("a", "b", "c", "d", "z"))
   }
 
   "WCOJ implementation" should "find the same two-paths as Spark's original joins" in {
@@ -93,6 +147,35 @@ class AmazonDatasetTriangleQuery extends FlatSpec with Matchers with SparkTest {
 
     val diff = a.rdd.subtract(e.rdd)
     diff.isEmpty() should be (true)
+  }
+
+  "WCOJ implementation" should "find the same three-paths as Spark's original joins" in {
+    val a = threePathPattern(ds, nodeSet1, nodeSet2).cache()
+    val e = threePathGoldStandard(ds, nodeSet1, nodeSet2).cache()
+
+    val diff = a.rdd.subtract(e.rdd)
+    diff.isEmpty() should be (true)
+    a.isEmpty should be (false)
+    e.isEmpty should be (false)
+  }
+
+  "WCOJ implementation" should "find the same four-paths as Spark's original joins" in {
+    val startA = System.nanoTime()
+    val a = fourPathPattern(ds, nodeSet1, nodeSet2).cache()
+    print("a ", a.count())
+    val endA = System.nanoTime() - startA
+    println("WCOJ four-path ", endA / 1000000000)
+
+    val startE = System.nanoTime()
+    val e = fourPathGoldStandard(ds, nodeSet1, nodeSet2).cache()
+    println("e", e.count())
+    val endE = System.nanoTime() - startE
+    println("Spark four-path ", endE / 1000000000)
+
+    val diff = a.rdd.subtract(e.rdd)
+    diff.isEmpty() should be (true)
+    a.isEmpty should be (false)
+    e.isEmpty should be (false)
   }
 
 
