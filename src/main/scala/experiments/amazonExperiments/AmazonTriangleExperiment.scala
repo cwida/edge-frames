307e9d4696186126f80024efb7297ac654ceaec8
Per Fuchs <per@shiftleft.io>
2019-04-30 17:20:04 +0200

    Repairs AmazonTriangleExperiment

--- src/main/scala/experiments/amazonExperiments/AmazonTriangleExperiment.scala
+++ src/main/scala/experiments/amazonExperiments/AmazonTriangleExperiment.scala
@@ -8,10 +8,9 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object AmazonTriangleExperiment extends App with GenericExperiment {
   override def loadDataset(sp: SparkSession) = loadAmazonDataset(sp)
 
-  // TODO correct, exchange runWCOJ and runBinaryJoins implmentation
-  override def runWCOJ(sp: SparkSession, dataSet: DataFrame) = triangleBinaryJoins(sp, dataSet).count()
+  override def runWCOJ(sp: SparkSession, dataSet: DataFrame) =  trianglePattern(dataSet).count()
 
-  override def runBinaryJoins(sp: SparkSession, dataSet: DataFrame) = trianglePattern(dataSet).count()
+  override def runBinaryJoins(sp: SparkSession, dataSet: DataFrame) =triangleBinaryJoins(sp, dataSet).count()
 
   run()
 }
