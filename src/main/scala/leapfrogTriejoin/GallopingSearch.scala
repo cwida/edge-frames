8e037fd68ab66ed68e04482f65bf53ba73b0423b
Per Fuchs <per@shiftleft.io>
2019-05-03 18:15:09 +0200

    Optimizes binary search, use linear search, uses one less branch in loop.
    WCOJ, amazon0602, 5-clique 119 --> 105

--- src/main/scala/leapfrogTriejoin/GallopingSearch.scala
+++ src/main/scala/leapfrogTriejoin/GallopingSearch.scala
@@ -6,6 +6,9 @@ import org.apache.spark.sql.vectorized.ColumnVector
 
 object GallopingSearch {
 
+  private val LINEAR_SEARCH_THRESHOLD = 60
+
+  // TODO could be used with summary
   def find(values: ColumnVector, key: Int, start: Int, end: Int): Int = {
     assert(end != 0)
     assert(start < end)
@@ -14,35 +17,51 @@ object GallopingSearch {
     while (bound < end && values.getInt(bound) < key) {
       bound *= 2
     }
-    // TODO if less than 50 or 60 go linear
     binarySearch(values, key, Math.max(start, bound / 2), min(bound + 1, end))
   }
 
+  def linearSearch(vector: ColumnVector, key: Int, start: Int, end: Int): Int = {
+    var pos = start
+    while (pos < end && vector.getInt(pos) < key) {
+      pos += 1
+    }
+    pos
+  }
+
+  // TODO write more unit tests about this
   def binarySearch(vector: ColumnVector, key: Int, start: Int, end: Int): Int = {
     assert(0 <= start)
     assert(start < end)
 
-    // TODO could be used with summary
-
     var L = start
     var R = end
-    while (L < R) {
-      var M: Int = (L + R) / 2
+    var M = -1
+    while (L < R - LINEAR_SEARCH_THRESHOLD) {
+      M = (L + R) >> 1  // x >> 1 === x / 2
+
       if (vector.getInt(M) < key) {
         L = M + 1
-      } else if (vector.getInt(M) > key) {
+      } else if (vector.getInt(M) >= key) {
         R = M - 1
-      } else {
-        while (start < M && vector.getInt(M - 1) == key) {
-          M -= 1
-        }
-        return M
       }
     }
-    while (start < L && vector.getInt(L - 1) == key) {
-      L -= 1
+
+    if (L < R) {
+      linearSearch(vector, key, L, Math.min(R + 1, end))
+    } else {
+      if (vector.getInt(M) == key) { // 105 --> 99
+        if (start < M && vector.getInt(M - 1) == key) {
+          M -= 1
+        }
+        M
+      } else {
+        Math.min(if (L < end && vector.getInt(L) < key) {
+          L + 1
+        } else {
+          L
+        }, end)
+      }
     }
-    Math.min(if (L < end && vector.getInt(L) < key) { L + 1} else { L }, end)
   }
 
 }
